bb906ba589640d2dd13b4729206e3174f01c3421
birhatkaya <birhatkayaa@hotmail.com>
2022-10-10 12:19:15 +0300

    Hooks
    asdsd

--- App.js
+++ App.js
@@ -1,20 +1,41 @@
 import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React, { useState} from 'react';
+import { SafeAreaView, StyleSheet, Text, View, Image, TouchableWithoutFeedback, TouchableHighlight, TouchableOpacity } from 'react-native';
 
 export default function App() {
+
+  const [visible, setVisible] = useState(true)
+
+  const toggleView = () => {
+    setVisible(!visible);
+  }
+
   return (
     <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
+        <TouchableOpacity onPress={toggleView} activeOpacity={0.7}>
+        <View style={styles.button}>
+          <Text style={{color: '#fff'}}>{visible ? 'Gizle' : 'Göster'}</Text>
+        </View>   
+        </TouchableOpacity>
+        <View style={{display: visible ? 'flex' : 'none'}}>
+          <Text>bunu ya gördün ya da görmedin</Text>
+        </View>
     </View>
   );
 }
 
 const styles = StyleSheet.create({
   container: {
+    justifyContent: 'center',
+    alignItems: 'center',
     flex: 1,
-    backgroundColor: '#fff',
+    backgroundColor: '#fff'
+  },
+  button: {
+    backgroundColor: 'red',
+    heigt: 50,
+    width: 200,
     alignItems: 'center',
     justifyContent: 'center',
-  },
+  }
 });
